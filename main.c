5035edc1b39a07c79ebb0171b845bb8cafced4c6
wcz123694578 <wcz123694578@outlook.com>
2024-12-15 06:25:40 +0000

    支持条件运算符

--- main.c
+++ main.c
@@ -18,6 +18,11 @@ typedef enum {
     ND_MUL, // *
     ND_DIV, // /
     ND_NUM, // 整形
+    ND_NEG,
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
 } NodeKind;
 
 typedef struct Token Token;
@@ -91,6 +96,22 @@ static Token *skip(Token *Tok, char *Str) {
     return Tok->Next;
 }
 
+static bool startsWith(char *Str, char *SubStr) {
+    return strncmp(Str, SubStr, strlen(SubStr)) == 0;
+}
+
+static int readPunct(char *Ptr) {
+    if (    startsWith(Ptr, "==") 
+        ||  startsWith(Ptr, "!=")
+        ||  startsWith(Ptr, "<=")
+        ||  startsWith(Ptr, ">="))
+    {
+        return 2;    
+    }
+
+    return ispunct(*Ptr) ? 1 : 0;
+}
+
 static int getNumber(Token *Tok) {
     if (Tok->Kind != TK_NUM)
         error("expect a number");
@@ -108,6 +129,13 @@ static Node *newBinary(NodeKind Kind, Node *LHS, Node *RHS) {
     Node *Nd = newNode(Kind);
     Nd->LHS = LHS;
     Nd->RHS = RHS;
+    return Nd;
+}
+
+// 新建一个单叉树
+static Node *newUnary(NodeKind Kind, Node *Expr) {
+    Node *Nd = newNode(Kind);
+    Nd->LHS = Expr;
     return Nd;
 }
 // 新建一个数字节点
@@ -138,10 +166,11 @@ static Token *tokenize() {
             continue;
         }
 
-        if (ispunct(*P)) {
-            Cur->Next = newToken(TK_PUNCT, P, P + 1);
+        int PunctLen = readPunct(P);
+        if (PunctLen) {
+            Cur->Next = newToken(TK_PUNCT, P, P + PunctLen);
             Cur = Cur->Next;
-            ++P;
+            P += PunctLen;
             continue;
         }
 
@@ -152,14 +181,91 @@ static Token *tokenize() {
     return Head.Next;
 }
 
+
+// expr = equality
+// equality = relational ("==" relational | "!=" relational)*
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+// add = mul ("+" mul | "-" mul)*
+// mul = unary ("*" unary | "/" unary)*
+// unary = ("+" | "-") unary | primary
+// primary = "(" expr ")" | num
 static Node *expr(Token **Rest, Token *Tok);
 static Node *mul(Token **Rest, Token *Tok);
 static Node *primary(Token **Rest, Token *Tok);
+static Node *unary(Token **Rest, Token *Tok);
+static Node *equality(Token **Rest, Token *Tok);
+static Node *relational(Token **Rest, Token *Tok);
+static Node *add(Token **Rest, Token *Tok);
 // 解析加减
 // expr = mul ("+" mul | "-" mul)*
 static Node *expr(Token **Rest, Token *Tok) {
+    return equality(Rest, Tok);
+}
+
+static Node *equality(Token **Rest, Token *Tok) {
+    Node *Nd = relational(&Tok, Tok);
+    // ("+" mul | "-" mul)*
+    while (true) {
+        // "+" mul
+        if (equal(Tok, "==")) {
+            Nd = newBinary(ND_EQ, Nd, relational(&Tok, Tok->Next));
+            continue;
+        }
+        // "-" mul
+        if (equal(Tok, "!=")) {
+            Nd = newBinary(ND_NE, Nd, relational(&Tok, Tok->Next));
+            continue;
+        }
+        *Rest = Tok;
+        return Nd;
+    }
+}
+
+// 解析比较关系
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+static Node *relational(Token **Rest, Token *Tok) {
+    // add
+    Node *Nd = add(&Tok, Tok);
+
+    // ("<" add | "<=" add | ">" add | ">=" add)*
+    while (true) {
+        // "<" add
+        if (equal(Tok, "<")) {
+        Nd = newBinary(ND_LT, Nd, add(&Tok, Tok->Next));
+        continue;
+        }
+
+        // "<=" add
+        if (equal(Tok, "<=")) {
+        Nd = newBinary(ND_LE, Nd, add(&Tok, Tok->Next));
+        continue;
+        }
+
+        // ">" add
+        // X>Y等价于Y<X
+        if (equal(Tok, ">")) {
+        Nd = newBinary(ND_LT, add(&Tok, Tok->Next), Nd);
+        continue;
+        }
+
+        // ">=" add
+        // X>=Y等价于Y<=X
+        if (equal(Tok, ">=")) {
+        Nd = newBinary(ND_LE, add(&Tok, Tok->Next), Nd);
+        continue;
+        }
+
+        *Rest = Tok;
+        return Nd;
+    }
+}
+
+// 解析加减
+// add = mul ("+" mul | "-" mul)*
+static Node *add(Token **Rest, Token *Tok) {
     // mul
     Node *Nd = mul(&Tok, Tok);
+
     // ("+" mul | "-" mul)*
     while (true) {
         // "+" mul
@@ -167,36 +273,51 @@ static Node *expr(Token **Rest, Token *Tok) {
         Nd = newBinary(ND_ADD, Nd, mul(&Tok, Tok->Next));
         continue;
         }
+
         // "-" mul
         if (equal(Tok, "-")) {
         Nd = newBinary(ND_SUB, Nd, mul(&Tok, Tok->Next));
         continue;
         }
+
         *Rest = Tok;
         return Nd;
     }
 }
+
 // 解析乘除
 // mul = primary ("*" primary | "/" primary)*
 static Node *mul(Token **Rest, Token *Tok) {
-    // primary
-    Node *Nd = primary(&Tok, Tok);
-    // ("*" primary | "/" primary)*
+    // unary
+    Node *Nd = unary(&Tok, Tok);
     while (true) {
-        // "*" primary
+        // "*" unary
         if (equal(Tok, "*")) {
-        Nd = newBinary(ND_MUL, Nd, primary(&Tok, Tok->Next));
+        Nd = newBinary(ND_MUL, Nd, unary(&Tok, Tok->Next));
         continue;
         }
-        // "/" primary
+        // "/" unary
         if (equal(Tok, "/")) {
-        Nd = newBinary(ND_DIV, Nd, primary(&Tok, Tok->Next));
+        Nd = newBinary(ND_DIV, Nd, unary(&Tok, Tok->Next));
         continue;
         }
         *Rest = Tok;
         return Nd;
     }
 }
+
+static Node *unary(Token **Rest, Token *Tok) {
+    if (equal(Tok, "+")) {
+        return unary(Rest, Tok->Next);
+    }
+
+    if (equal(Tok, "-")) {
+        return newUnary(ND_NEG, unary(Rest, Tok->Next));
+    }
+
+    return primary(Rest, Tok);
+}
+
 // 解析括号、数字
 // primary = "(" expr ")" | num
 static Node *primary(Token **Rest, Token *Tok) {
@@ -225,22 +346,32 @@ static int Depth;
 // 当前栈指针的地址就是sp，将a0的值压入栈
 // 不使用寄存器存储的原因是因为需要存储的值的数量是变化的。
 static void push(void) {
-    printf("  addi sp, sp, -8\n");
-    printf("  sd a0, 0(sp)\n");
+    printf("    addi sp, sp, -8\n");
+    printf("    sd a0, 0(sp)\n");
     Depth++;
 }
 // 弹栈，将sp指向的地址的值，弹出到a1
 static void pop(char *Reg) {
-    printf("  ld %s, 0(sp)\n", Reg);
-    printf("  addi sp, sp, 8\n");
+    printf("    ld %s, 0(sp)\n", Reg);
+    printf("    addi sp, sp, 8\n");
     Depth--;
 }
 // 生成表达式
 static void genExpr(Node *Nd) {
     // 加载数字到a0
-    if (Nd->Kind == ND_NUM) {
-        printf("  li a0, %d\n", Nd->Val);
+    switch (Nd->Kind) {
+
+    case ND_NUM:
+        printf("    li a0, %d\n", Nd->Val);
         return;
+
+    case ND_NEG:
+        genExpr(Nd->LHS);
+        printf("    neg a0, a0\n");
+        return;
+    
+    default:
+        break;
     }
     // 递归到最右节点
     genExpr(Nd->RHS);
@@ -253,16 +384,40 @@ static void genExpr(Node *Nd) {
     // 生成各个二叉树节点
     switch (Nd->Kind) {
     case ND_ADD: // + a0=a0+a1
-        printf("  add a0, a0, a1\n");
+        printf("    add a0, a0, a1\n");
         return;
     case ND_SUB: // - a0=a0-a1
-        printf("  sub a0, a0, a1\n");
+        printf("    sub a0, a0, a1\n");
         return;
     case ND_MUL: // * a0=a0*a1
-        printf("  mul a0, a0, a1\n");
+        printf("    mul a0, a0, a1\n");
         return;
     case ND_DIV: // / a0=a0/a1
-        printf("  div a0, a0, a1\n");
+        printf("    div a0, a0, a1\n");
+        return;
+    case ND_EQ:
+    case ND_NE:
+        // a0=a0^a1，异或指令
+        printf("    xor a0, a0, a1\n");
+        if (Nd->Kind == ND_EQ)
+        // a0==a1
+        // a0=a0^a1, sltiu a0, a0, 1
+        // 等于0则置1
+        printf("    seqz a0, a0\n");
+        else
+        // a0!=a1
+        // a0=a0^a1, sltu a0, x0, a0
+        // 不等于0则置1
+        printf("    snez a0, a0\n");
+        return;
+    case ND_LT:
+        printf("    slt a0, a0, a1\n");
+        return;
+    case ND_LE:
+        // a0<=a1等价于
+        // a0=a1<a0, a0=a1^1
+        printf("    slt a0, a1, a0\n");
+        printf("    xori a0, a0, 1\n");
         return;
     default:
         break;
--- test.sh
+++ test.sh
@@ -43,6 +43,36 @@ assert 41 ' 12 + 34 - 5 '
 
 # [4] 支持* /
 assert 116 '30 * 4 - 20 / 5'
+assert 15 '5*(9-6)'
+assert 17 '1-8/(2*2)+3*6'
+
+# [5] 支持一元运算的+ -
+assert 10 '-10+20'
+assert 10 '- -10'
+assert 10 '- - +10'
+assert 48 '------12*+++++----++++++++++4'
+
+# [6] 支持关系运算符
+assert 0 '0==1'
+assert 1 '42==42'
+assert 1 '0!=1'
+assert 0 '42!=42'
+assert 1 '0<1'
+assert 0 '1<1'
+assert 0 '2<1'
+assert 1 '0<=1'
+assert 1 '1<=1'
+assert 0 '2<=1'
+assert 1 '1>0'
+assert 0 '1>1'
+assert 0 '1>2'
+assert 1 '1>=0'
+assert 1 '1>=1'
+assert 0 '1>=2'
+assert 1 '5==2+3'
+assert 0 '6==4+3'
+assert 1 '0*9+5*2==4+4*(6/3)-2'
+
 
 # 如果运行正常未提前退出，程序将显示OK
 echo OK
